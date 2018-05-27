fed98582850b9ece521d2537ba61a2d5cdbc4497
Vytautas Saulis <vsaulis@trackforce.com>
2018-05-27 17:44:17 +0300

    Add project files.

---
+++ DeepFirstSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeepFirstSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
